ede081568a02d22dec7ffd877f6a281539f723bd
felix-kocht <159034498+felix-kocht@users.noreply.github.com>
2025-01-12 18:51:58 +0100

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: mode === "production" ? "/felix-kocht/" : "/",
+  base: "/", // Use "/" for a custom domain
   plugins: [
     react(),
     mode === "development" && componentTagger(),
